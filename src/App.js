56248ea5b5dce09712099b801cd3b6b4f4c4e212
Tim Sansone <tim.sansone@Tim-Sansone.local>
2022-06-23 14:20:22 -0500

    edit function operational

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
-
 import { Route, Switch, Redirect } from "react-router-dom";
+import axios from 'axios';
+
 import MovieList from './components/MovieList';
 import Movie from './components/Movie';
-
 import MovieHeader from './components/MovieHeader';
-
 import FavoriteMovieList from './components/FavoriteMovieList';
+import EditMovieForm from "./components/EditMovieForm";
 
-import axios from 'axios';
+
 
 const App = (props) => {
   const [movies, setMovies] = useState([]);
@@ -44,6 +44,7 @@ const App = (props) => {
         
           <Switch>
             <Route path="/movies/edit/:id">
+              <EditMovieForm setMovies={setMovies}/>
             </Route>
 
             <Route path="/movies/:id">
